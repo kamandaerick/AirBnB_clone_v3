84cd814e580655af861d3f4caa0d55f864a4f59e
Erick Kamanda <info.proresearchers@gmail.com>
2024-08-04 20:06:55 +0300

    Custom 404 Not found page

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,14 +10,22 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close_db(obj):
+    """ calls methods close() """
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_foun(error):
+    """ Loads a custom 404 page not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=port, threaded=True)
+
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
